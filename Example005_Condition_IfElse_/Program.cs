c7d4777dc332c8631d670b425422d125badb22a1
kyrylozo <kyzltk@gmail.com>
2023-01-27 11:36:01 +0100

    Add Example 005 Condition If-Else

---
+++ Example005_Condition_IfElse_/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") //Благодаря "ToLower" уходит чувствительность к реестру букв в слове "Маша"
+{
+Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
