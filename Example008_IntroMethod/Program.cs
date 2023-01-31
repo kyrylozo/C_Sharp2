c9632e1fbf7cbd85ecc0ddfb712b29a49b02c199
kyrylozo <kyzltk@gmail.com>
2023-01-31 20:24:35 +0100

    Классический, стихийный подход к выявлению переменной с максимальным значением

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿// 
+
+int a1 = 15,
+    b1 = 21,
+    c1 = 39,
+    a2 = 1211,
+    b2 = 23,
+    c2 = 33,
+    a3 = 13,
+    b3 = 23,
+    c3 = 313;
+
+    int max = a1;
+    if(b1 > max) max = b1;
+    if(c1 > max) max = c1;
+
+    if(a2 > max) max = a2;
+    if(b2 > max) max = b2;
+    if(c2 > max) max = c2;
+
+    if(a3 > max) max = a3;
+    if(b3 > max) max = b3;
+    if(c3 > max) max = c3;
+  
+  Console.WriteLine(max);
