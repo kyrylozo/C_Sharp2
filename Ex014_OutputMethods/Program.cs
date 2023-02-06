3491a4ad065ded5f1e922724d2546a601cb84393
kyrylozo <kyzltk@gmail.com>
2023-02-06 17:28:21 +0100

    Методы с выведением результата действия

---
+++ Ex014_OutputMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1. Получение данных без введения в терминал
+// 2. Введение текста заданное количество раз
+
+//int Time()
+{
+    //return DateTime.Now.Year;
+}
+//Console.WriteLine(Time());
+
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "Text ");
+Console.WriteLine (res);
