be263723e9fbd2ffa28edc62b2c5a075d3cde11d
kyrylozo <kyzltk@gmail.com>
2023-02-06 15:26:04 +0100

    Метод для повторения вводимого текста заданное число раз

---
+++ Ex013_Text_x_Times/Program.cs
@@ -0,0 +1,18 @@
+﻿// Метод для повторения заданного текста заданное число раз
+
+void tMethod(string msg, byte count)
+{
+    byte i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+//tMethod(msg: "Текст", count: 4);
+tMethod(count: 4, msg: "Текст"); // При введении названий переменных, последовательность можно менять
+
+// tMethod("Hellow everybody", 4);
+
+
