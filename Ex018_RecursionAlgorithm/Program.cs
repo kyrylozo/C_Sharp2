85b0f924ba69fc9057d07b036434e85a7ebdf6eb
kyrylozo <kyzltk@gmail.com>
2023-02-16 07:17:18 +0100

    Создание пустого двумерного массива

---
+++ Ex018_RecursionAlgorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создание пустого двумерного массива
+
+
+// string[,] table = new string[2, 5];
+// //        Для строк инициализация производится: "String.Empty"
+// //       table[0,0]  table[0,1].  table[0,2] ... table[0,4]
+// //       table[1,0]  table[1,1].  table[1,2] ... table[1,4]
+// table[1, 2] = "word";
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 5; columns++)
+//     {
+//     Console.WriteLine($"- {table[rows, columns]}-");
+//     }
+// }
+
+int[,] matrix = new int[3, 6];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i,j]}");
+    }
+    Console.WriteLine();
+}
