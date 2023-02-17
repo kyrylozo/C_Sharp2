5df783799b980bd318f93d43f8f962e50ca42791
kyrylozo <kyzltk@gmail.com>
2023-02-17 17:37:19 +0100

    Метод рекурсии для чисел Фибоначчи

---
+++ Ex021_RecursionFibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿// Метод рекурсии для чисел Фибоначчи
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f (n - 2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for(int i = 1; i < 50; i ++)
+{
+    Console.WriteLine($"f({i}): {Fibonacci(i)}");
+}
+
+// int Fibonacci(int n)
+// {
+//     if(n == 1 || n == 2 || n == 3) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 3);
+// }
+
+// for(int i = 1; i < 33; i ++)
+// {
+//     Console.WriteLine($"n = {i}: {Fibonacci(i)}");
+// }
