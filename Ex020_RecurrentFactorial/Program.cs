8fb4e374009f31e3d9e99f1540f4bac2d0b9069e
kyrylozo <kyzltk@gmail.com>
2023-02-17 16:59:44 +0100

    Рекурсивное вычисление факториала

---
+++ Ex020_RecurrentFactorial/Program.cs
@@ -0,0 +1,11 @@
+﻿// Рекурсивное вычисление факториала
+
+double Factorial (int n) // double используется, так как в результате появляются
+//                          сллишком огромные числа для int
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1 || n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(39));
