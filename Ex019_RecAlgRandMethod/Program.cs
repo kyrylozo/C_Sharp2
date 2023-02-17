3467b71ccea03fabbfcd3b7730744eba62db6183
kyrylozo <kyzltk@gmail.com>
2023-02-17 05:18:50 +0100

    Создание таблицы и наполнение ее случайными числами

---
+++ Ex019_RecAlgRandMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿// Создание таблицы и наполнение ее случайными числами
+
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 10); // [1, 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 10]; // new int [строки (matr.GetLength(0)), столбцы (matr.GetLength(1))]
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
