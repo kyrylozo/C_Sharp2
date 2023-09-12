cd92e57b9586e65f16ff641e75594cf39c6187a8
kyrylozo <kyzltk@gmail.com>
2023-09-12 12:18:25 +0200

    Some improvisation with the code

--- Ex014_OutputMethods/Program.cs
+++ Ex014_OutputMethods/Program.cs
@@ -1,12 +1,16 @@
-﻿ // 1. Получение данных без введения в терминал
- // 2. Введение текста заданное количество раз
+﻿ // 3. Методы, которые ничего не принимают, но что-то возвращают
+ // Получение данных без введения в терминал
 
 int Time()
 {
     return DateTime.Now.Year;
 }
-Console.WriteLine(Time());
+int year = Time();
+Console.WriteLine(Time() - 2);
+Console.WriteLine(year + 1);
 
+// 4. Методы, которые что-то прирнимает и что-то возвращает
+ // Введение текста заданное количество раз
 
 string Method4(byte count, string text)
 {
@@ -14,10 +18,20 @@ string Method4(byte count, string text)
     for(byte i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
-string res = Method4(10, "z");
+string res = Method4(3, "Y");
 Console.WriteLine(res);
 
+string Method5(byte count, string text)
+{
+    string result = String.Empty;
+    for(; count > 0; count--)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string reslt = Method5(3, "Z");
+Console.WriteLine(reslt);
