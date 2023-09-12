5cd2b1315107dcc0f488fad1498876da68e30e84
kyrylozo <kyzltk@gmail.com>
2023-09-12 12:40:39 +0200

    Создание цикла в цикле (с модификациями)

--- Ex015_2xCycle/Program.cs
+++ Ex015_2xCycle/Program.cs
@@ -9,3 +9,12 @@ for ( int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+
+for(byte ix = 9; ix > 1; ix--)
+{
+    for(byte jx = 9; jx > 1; jx--)
+    {
+        System.Console.WriteLine($"{ix} x {jx} = {ix * jx}");
+    }
+    System.Console.WriteLine();
+}
